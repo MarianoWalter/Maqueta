589c1847d11461f5d0941fa81962239e727d057e
MarianoWalter <marianowalter.14@gmail.com>
2019-12-19 19:22:22 -0300

    Configuración

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+// CSS de Bootstrap 3
+import 'bootstrap3/dist/css/bootstrap.min.css';
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	render: h => h(App),
+}).$mount('#app');
